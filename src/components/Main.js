f491746e134470b9b6d1b16eec34cb35aa887c3c
Karol Grydz <grydzkarol@gmail.com>
2019-10-03 13:31:44 +0200

    REDUX Start

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 export class Main extends Component {
-  changeUsername = () => {
-    const { changeUsername } = this.props;
-    changeUsername('Anna');
-  };
+  // changeUsername = () => {
+  //   const { changeUsername } = this.props;
+  //   changeUsername('Anna');
+  // };
 
   render() {
+    const { changeUsername } = this.props;
     return (
       <div>
         <div className="row">
@@ -16,7 +17,10 @@ export class Main extends Component {
         </div>
         <div className="row">
           <div className="col-xs-12">
-            <button className="btn btn-primary" onClick={this.changeUsername}>
+            <button
+              className="btn btn-primary"
+              onClick={() => changeUsername('Anna')}
+            >
               Change the Username
             </button>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,30 +1,74 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
 
-import { User } from './components/User';
-import { Main } from './components/Main';
+// import { User } from './components/User';
+// import { Main } from './components/Main';
 
-class App extends Component {
-  state = {
-    userName: 'Max'
-  };
+// class App extends Component {
+//   state = {
+//     userName: 'Max'
+//   };
 
-  changeUsername = newName => {
-    this.setState({
-      userName: newName
-    });
-  };
+//   changeUsername = newName => {
+//     this.setState({
+//       userName: newName
+//     });
+//   };
 
-  render() {
-    const { userName } = this.state;
-    return (
-      <div className="container">
-        <h1>Hello world</h1>
-        <Main changeUsername={this.changeUsername} />
-        <User username={userName} />
-      </div>
-    );
+//   render() {
+//     const { userName } = this.state;
+//     return (
+//       <div className="container">
+//         <h1>Hello world</h1>
+//         <Main changeUsername={this.changeUsername} />
+//         <User username={userName} />
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import { createStore } from 'redux';
+
+//dostępne akcje które możemy wykonać
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      state = state + action.payload;
+      break;
+    case 'SUBTRACT':
+      state = state - action.payload;
+      break;
   }
-}
+  return state;
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Tworzenie naszego store z danamymi
+const store = createStore(reducer, 1);
+
+//Tutaj będą nasze aplikacje z reacta
+store.subscribe(() => {
+  console.log('State updated!', store.getState());
+});
+
+//Wybieram dostępną akcje z reducera i ją wysyłam
+store.dispatch({
+  type: 'ADD',
+  payload: 100
+});
+
+store.dispatch({
+  type: 'ADD',
+  payload: 100
+});
+
+store.dispatch({
+  type: 'ADD',
+  payload: 100
+});
+
+store.dispatch({
+  type: 'SUBTRACT',
+  payload: 100
+});
